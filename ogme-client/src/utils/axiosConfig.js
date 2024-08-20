54af128c6ac8a3e7d3c8a6e46227100185d05858
Muhammed Adham <136921297+muhammed-adham@users.noreply.github.com>
2024-08-21 02:09:25 +0300

    Port3000

--- ogme-client/src/utils/axiosConfig.js
+++ ogme-client/src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://localhost:3000",
   withCredentials: true,
 });
 export const request = ({ ...option }) => {
