f55af5ac0dec4a3c3357f31e6d2d96e95ebe2cf6
omaralzayan <omaralzayan@gmail.com>
2024-09-08 16:34:22 +0000

    currentrunning08092024

--- ogme-client/src/utils/axiosConfig.js
+++ ogme-client/src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  baseURL: "http://localhost:3000",
+	baseURL: "https://ogme.store/api",
   withCredentials: true,
 });
 export const request = ({ ...option }) => {
